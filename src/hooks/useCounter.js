2edb8b52b122eda0a393a48dd4f32b2c75c40aa4
César Vargas Júnior <cesar_vargas_jr@hotmail.com>
2021-12-03 15:52:37 -0300

    Módulo 4 - Hooks

---
+++ src/hooks/useCounter.js
@@ -0,0 +1,17 @@
+/* HOOK PERSONALIZADO - CONTADOR SIMPLES */
+
+import { useState } from 'react'
+
+export const useCounter = (initialValue = 10) => {
+    const [count, setCount] = useState(initialValue)
+
+    function inc() {
+        setCount(count + 1)
+    }
+
+    function dec() {
+        setCount(count - 1)
+    }
+
+    return [count, inc, dec]
+}
---
+++ src/hooks/useFetch.js
@@ -0,0 +1,21 @@
+/* HOOK PERSONALIZADO - BUSCAR DADOS DE API */
+
+import { useEffect, useState } from "react"
+
+export const useFetch = (url, method = 'get') => {
+    const [response, setResponse] = useState({
+        data: null,
+        loading: false
+    })
+
+    useEffect(function() {
+        fetch(url, { method })
+            .then(resp => resp.json())
+            .then(json => setResponse({
+                data: json,
+                loading: false
+            }))
+    }, [url, method])
+
+    return response
+}
